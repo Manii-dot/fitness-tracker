27efad8d4227298f17bd7236cc6e4b286b2cd29f
Emmanuel <59237112+Manii-dot@users.noreply.github.com>
2020-11-12 22:09:50 -0600

    require morgan

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
+
+require("./routes/api_route.js")(app);
+require("./routes/html_route.js")(app);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/exercise")
 app.listen(PORT, () => {
