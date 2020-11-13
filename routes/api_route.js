9bc91bde7d5ab039f2b5cf8a9640864dd4bf215e
Emmanuel <59237112+Manii-dot@users.noreply.github.com>
2020-11-12 21:31:28 -0600

    set up api route

---
+++ routes/api_route.js
@@ -0,0 +1,24 @@
+// const router = require("express").Router();
+var db = require("..models/workout.js");
+
+module.exports = function(app) {
+    app.get("/api/workouts", function(req, res){
+        db.find().then((data) => res.json(data))
+        .catch(err => res.json(err))
+    });
+    app.get("/api/workouts/range", function (req, res) {
+        db.find().then(data => res.json(data))
+        .catch(err => res.json(err))
+    });
+    app.post("/api/workouts", function(req, res) {
+        db.create({}).then((data) => res.json(data))
+        .catch(err => res.json(err))
+    });
+    app.put("/api/workouts/:id", function(req, res){
+        db.findByIdAndUpdate(req.params.id, {
+            $push: {exercises: req.body}
+        })
+        .then((data) => res.json(data))
+        .catch(err => res.json(err))
+    })
+}
